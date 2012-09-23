d80d9b543f9a4f8ef842828e67b176432f349bd6
Geoff Greer <geoff@greer.fm>
2012-09-23 07:26:53 -0700

    Add logging stuff

---
+++ src/log.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "log.h"
+
+static enum log_level log_threshold = LOG_LEVEL_ERR;
+
+void set_log_level(enum log_level threshold) {
+    log_threshold = threshold;
+}
+
+/* Maybe these should be macros? */
+void log_debug(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_DEBUG, fmt, args);
+    va_end(args);
+}
+
+void log_msg(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_MSG, fmt, args);
+    va_end(args);
+}
+
+void log_warn(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_WARN, fmt, args);
+    va_end(args);
+}
+
+void log_err(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_ERR, fmt, args);
+    va_end(args);
+}
+
+void vplog(const unsigned int level, const char *fmt, va_list args) {
+    if (level < log_threshold) {
+        return;
+    }
+
+    FILE *stream = stdout;
+
+    switch(level) {
+        case LOG_LEVEL_DEBUG:
+            fprintf(stream, "DEBUG: ");
+        break;
+        case LOG_LEVEL_MSG:
+            fprintf(stream, "MSG: ");
+        break;
+        case LOG_LEVEL_WARN:
+            fprintf(stream, "WARN: ");
+        break;
+        case LOG_LEVEL_ERR:
+            stream = stderr;
+            fprintf(stream, "ERR: ");
+        break;
+    }
+
+    vfprintf(stream, fmt, args);
+    fprintf(stream, "\n");
+}
+
+void plog(const unsigned int level, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(level, fmt, args);
+    va_end(args);
+}
---
+++ src/log.h
@@ -0,0 +1,23 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdarg.h>
+
+enum log_level {
+    LOG_LEVEL_DEBUG = 10,
+    LOG_LEVEL_MSG   = 20,
+    LOG_LEVEL_WARN  = 30,
+    LOG_LEVEL_ERR   = 40
+};
+
+void set_log_level(enum log_level threshold);
+
+void log_debug(const char *fmt, ...);
+void log_msg(const char *fmt, ...);
+void log_warn(const char *fmt, ...);
+void log_err(const char *fmt, ...);
+
+void vplog(const unsigned int level, const char *fmt, va_list args);
+void plog(const unsigned int level, const char *fmt, ...);
+
+#endif
