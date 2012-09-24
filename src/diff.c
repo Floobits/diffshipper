5109fd1697e036c020022b8a149fe09b5f73157d
Geoff Greer <geoff@greer.fm>
2012-09-23 17:08:58 -0700

    Start checking modified times

--- src/diff.c
+++ src/diff.c
@@ -1,4 +1,9 @@
+#include <dirent.h>
+#include <sys/stat.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
 
 #include "dmp.h"
 #include "dmp_pool.h"
@@ -7,22 +12,49 @@
 #include "file.h"
 #include "log.h"
 
+struct timespec now;
+
+int modified_filter(const struct dirent *dir) {
+    struct stat dir_info;
+    lstat(dir->d_name, &dir_info);
+    /* check it out if it's modified in the last 10 seconds */
+    if (dir_info.st_mtimespec.tv_sec > now.tv_sec - 10) {
+        return 1;
+    }
+    return 0;
+}
 
 void push_changes(const char *path) {
     char orig_base[] = "/tmp/fuck_yo_couch";
     char *orig_path;
+    int orig_path_len;
+    struct dirent **dir_list = NULL;
+    struct dirent *dir = NULL;
+    int results;
+    int i;
     dmp_diff *diff;
+    gettimeofday(&now, NULL);
 
-    int orig_path_len = strlen(orig_base) + strlen(path) + 1;
+    orig_path_len = strlen(orig_base) + strlen(path) + 1;
     orig_path = malloc(orig_path_len);
     strlcpy(orig_path, orig_base, orig_path_len);
     strlcat(orig_path, path, orig_path_len);
-    log_debug("Getting changes for %s", path);
-    diff = diff_files(orig_path, path);
-    log_debug("diff: ");
-    if (diff) {
-        dmp_diff_free(diff);
+
+    results = scandir(path, &dir_list, &modified_filter, &alphasort);
+    if (results == 0)
+    {
+        log_debug("No results found in directory %s", path);
     }
+
+    for (i = 0; i < results; i++) {
+        log_debug("Getting changes for %s", path);
+        diff = diff_files(orig_path, path);
+        log_debug("diff: ");
+        if (diff) {
+            dmp_diff_free(diff);
+        }
+    }
+
     free(orig_path);
 }
 
