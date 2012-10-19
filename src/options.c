3d0f2890e16adea2b39bdb77caaf4d5a2953dfbb
Geoff Greer <geoff@greer.fm>
2012-10-19 16:37:01 -0700

    Forgot to add opts to getopt_long call

--- src/options.c
+++ src/options.c
@@ -42,7 +42,7 @@ void parse_opts(int argc, char **argv) {
         { NULL, 0, NULL, 0 }
     };
 
-    while ((ch = getopt_long(argc, argv, "Dh:p:v", longopts, &opt_index)) != -1) {
+    while ((ch = getopt_long(argc, argv, "Dh:p:r:s:u:v", longopts, &opt_index)) != -1) {
         switch (ch) {
             case 'D':
                 set_log_level(LOG_LEVEL_DEBUG);
