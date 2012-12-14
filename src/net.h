8699543b30ca0cd136d5ca670143704a5e6b7cad
Geoff Greer <geoff@greer.fm>
2012-12-14 11:04:27 -0800

    Remind myself of a way to reduce code

--- src/net.h
+++ src/net.h
@@ -19,6 +19,8 @@ int server_connect(const char *host, const char *port);
 ssize_t send_bytes(const void *buf, const size_t len);
 ssize_t recv_bytes(char **buf);
 
+/* TODO: send/recv_json functions */
+
 void *remote_change_worker();
 
 void net_cleanup();
