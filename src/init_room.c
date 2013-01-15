105d3d48e574fd45495fd9004e568acb74b19996
Geoff Greer <geoff@greer.fm>
2013-01-15 15:00:02 -0800

    Limit max search depth. This is pretty close to actually working.

--- src/init_room.c
+++ src/init_room.c
@@ -4,9 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include "init_room.h"
 #include "log.h"
+#include "mmap.h"
+#include "net.h"
 #include "options.h"
 #include "scandir.h"
 #include "util.h"
@@ -21,26 +24,31 @@ static int scandir_filter(const char *path, const struct dirent *d, void *baton)
 }
 
 
-void recurse_create_bufs(char *full_path) {
+void recurse_create_bufs(char *full_path, int depth) {
     struct dirent **dir_list = NULL;
     struct dirent *dir = NULL;
-    const char *base_path = opts.path;
     int results;
     int i;
     int rv;
-    char *path;
-    const char *path_start = full_path;
+    char *rel_path;
 
-    if (strlen(base_path) > strlen(full_path)) {
-        die("Base path %s is longer than full path %s!", base_path, full_path);
-    } else if (strcmp(base_path, full_path) == 0) {
-        log_debug("base path and full path are both %s", base_path);
-        path = strdup(base_path);
-    } else {
-        /* TODO: write a full path -> rel path helper function */
-        path = strdup(strlen(base_path)); /* skip last char and trailing slash */
+    if (depth > MAX_SEARCH_DEPTH) {
+        log_err("Exceeded max depth of %i. Not adding files in %s", MAX_SEARCH_DEPTH, full_path);
+        return;
     }
-    log_debug("path is %s", path);
+
+    if (strlen(opts.path) > strlen(full_path)) {
+        die("Base path %s is longer than full path %s!", opts.path, full_path);
+    } else if (strcmp(opts.path, full_path) == 0) {
+        log_debug("base path and full path are both %s", opts.path);
+        rel_path = strdup("");
+    } else if (strncmp(opts.path, full_path, strlen(opts.path)) != 0) {
+        die("wtf? %s != %s len %li", opts.path, full_path, strlen(opts.path));
+        return;
+    } else {
+        rel_path = strdup(full_path + strlen(opts.path) + 1); /* skip last char and trailing slash */
+    }
+    log_debug("path is %s", rel_path);
 
     results = ds_scandir(full_path, &dir_list, &scandir_filter, &full_path);
     if (results == -1) {
@@ -53,10 +61,56 @@ void recurse_create_bufs(char *full_path) {
 
     char *file_path;
     char *file_path_rel;
+    mmapped_file_t *mf;
+    struct stat dir_info;
+    struct stat file_info;
+    char *escaped_buf;
+
     for (i = 0; i < results; i++) {
         dir = dir_list[i];
-        ds_asprintf(&file_path, "%s%s", full_path, dir->d_name);
-        ds_asprintf(&file_path_rel, "%s%s", path, dir->d_name);
+        ds_asprintf(&file_path, "%s/%s", full_path, dir->d_name);
+        ds_asprintf(&file_path_rel, "%s%s", rel_path, dir->d_name);
         log_debug("file %s", file_path_rel);
+
+        /* If a link points to a directory then we need to treat it as a directory. */
+        if (dir->d_type == DT_LNK) {
+            if (stat(file_path, &dir_info) == -1) {
+                log_err("stat() failed on %s", file_path);
+                /* If stat fails we may as well carry on and hope for the best. */
+            } else if (S_ISDIR(dir_info.st_mode)) {
+                dir->d_type = DT_DIR;
+            }
+        }
+        if (dir->d_type == DT_DIR) {
+            recurse_create_bufs(file_path, depth + 1);
+            goto cleanup;
+        }
+
+        rv = lstat(file_path, &file_info);
+        if (rv)
+            die("Error lstat()ing file %s.", file_path);
+
+        mf = mmap_file(file_path, file_info.st_size, 0, 0);
+        if (is_binary(mf->buf, mf->len)) {
+            log_debug("%s is binary. skipping", file_path);
+        } else {
+            escaped_buf = escape_data(mf->buf, mf->len);
+            send_json(
+                "{s:s s:s s:s}",
+                "name", "create_buf",
+                "path", file_path_rel,
+                "buf", escaped_buf
+            );
+            free(escaped_buf);
+        }
+
+        munmap_file(mf);
+        free(mf);
+
+        cleanup:;
+        free(file_path_rel);
+        free(file_path);
     }
+
+    free(rel_path);
 }
--- src/init_room.h
+++ src/init_room.h
@@ -1,6 +1,8 @@
 #ifndef INIT_ROOM_H
 #define INIT_ROOM_H
 
-void recurse_create_bufs(char *full_path);
+#define MAX_SEARCH_DEPTH 25
+
+void recurse_create_bufs(char *full_path, int depth);
 
 #endif
